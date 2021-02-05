5eb5c54a3db403fb1cbdec9417162e21e2c72c10
ErikXJohansson <Erik.X.Johansson@hm.com>
2021-02-05 15:03:15 +0100

    New calculation method 3 times three

--- src/january/Main.java
+++ src/january/Main.java
@@ -3,11 +3,19 @@ package january;
 public class Main {
 
     public static void main(String[] args) {
-        int num = 3; // entered by user
+        int num = 3; // entered by user through scanner
         int multiplyingByThree = adding(num);
         System.out.println("Value entered multiplied by 3: " + multiplyingByThree + ".");
+        int multiplyingByThreeTimesThree = calculation(num);
+        System.out.println("Value entered multiplied by 3 times 3: " + multiplyingByThreeTimesThree + ".");
+
     }
-    public static int adding (int number) {
-        return number*3;
+
+    public static int adding(int number) {
+        return number * 3;
+    }
+
+    public static int calculation(int number) {
+        return number * 3 * 3 * 3;
     }
 }
